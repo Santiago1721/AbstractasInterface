8676c7a0a0f00d243cef3593be2e70ca1719bf51
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-02-08 13:56:52 +0100

    Create Electrodomestico.java

---
+++ Electrodomestico.java
@@ -0,0 +1,28 @@
+package abstractasinterfaz;
+
+class Electrodomesticosinterface implements ProductosInterface {
+	
+	private String marca;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	
+	public Electrodomesticosinterface(String nombre,String descripcion,double precio,String marca) {
+		this.marca = marca;
+	}
+	
+
+   
+	public void MostrarInformacion() {
+		System.out.println("el nombre es " +nombre);
+		System.out.println("la descripcion es " +descripcion);
+		System.out.println("el precio es " +precio);
+		System.out.println("su autor es " +marca);
+		
+	}
+	
+	
+	
+
+}
+	
